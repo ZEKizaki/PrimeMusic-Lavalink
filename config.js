b1ef031524974bbc27f2ccb3d93ae190bc126496
ZEKizaki <zekizaki2011@gmail.com>
2025-03-21 06:23:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["1003117516668813393", ""], 
+  mongodbUri : "mongodb+srv://axo:axoop@cluster0.imckmpc.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "bf5ee2a72bae40ffadc71a47280e5ff9",
+  spotifyClientSecret : "053469ffeb3844079fab734ebf30902",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
